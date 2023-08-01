eb8abc33e912696eaf4b06e560eb92dfb7eaab09
Yu Wu <yw3748@columbia.edu>
2023-08-01 11:29:33 -0400

    cc

--- server/app.py
+++ server/app.py
@@ -7,13 +7,12 @@ from flask import Flask, make_response, jsonify, request
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -21,9 +20,104 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return ''
+    return "Homepage"
 
-if __name__ == '__main__':
+
+@app.route("/campers", methods=["GET", "POST"])
+def campers():
+    if request.method == "GET":
+        return make_response(
+            [
+                camper.to_dict(only=("id", "name", "age"))
+                for camper in Camper.query.all()
+            ],
+            200,
+        )
+
+    elif request.method == "POST":
+        data = request.json
+        try:
+            camper = Camper(name=data["name"], age=data["age"])
+            db.session.add(camper)
+            db.session.commit()
+            return make_response(camper.to_dict(), 201)
+        except Exception as e:
+            return make_response({"errors": [str(e)]}, 400)
+
+
+@app.route("/campers/<int:id>", methods=["GET", "PATCH", "DELETE"])
+def camper_by_id(id):
+    camper = Camper.query.filter(Camper.id == id).first()
+    if camper:
+        if request.method == "GET":
+            return make_response(camper.to_dict(), 200)
+
+        elif request.method == "PATCH":
+            try:
+                data = request.json
+                for attr in data:
+                    setattr(camper, attr, data[attr])
+                    db.session.commit()
+                return make_response(camper.to_dict(), 202)
+            except Exception as e:
+                return make_response({"errors": [str(e)]}, 400)
+    else:
+        return make_response({"error": "Camper not found"}, 404)
+
+
+@app.route("/activities")
+def activities():
+    return make_response(
+        [
+            activity.to_dict(only=("id", "name", "difficulty"))
+            for activity in Activity.query.all()
+        ],
+        200,
+    )
+
+
+@app.route("/activities/<int:id>", methods=["GET", "DELETE"])
+def activity_by_id(id):
+    activity = Activity.query.filter(Activity.id == id).first()
+    if activity:
+        if request.method == "GET":
+            return make_response(activity.to_dict(), 200)
+
+        elif request.method == "DELETE":
+            for signup in activity.signups:
+                db.session.delete(signup)
+                db.session.commit()
+            db.session.delete(activity)
+            db.session.commit
+            return make_response("", 204)
+    else:
+        return make_response({"error": "Activity not found"}, 404)
+
+
+@app.route("/signups", methods=["GET", "POST"])
+def signups():
+    if request.method == "GET":
+        return make_response(
+            [signup.to_dict() for signup in Signup.query.all()],
+            200,
+        )
+
+    elif request.method == "POST":
+        data = request.json
+        try:
+            signup = Signup(
+                camper_id=data["camper_id"],
+                activity_id=data["activity_id"],
+                time=data["time"],
+            )
+            db.session.add(signup)
+            db.session.commit()
+            return make_response(signup.to_dict(), 201)
+        except Exception as e:
+            return make_response({"errors": [str(e)]}, 400)
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -9,7 +9,7 @@ convention = {
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 
 metadata = MetaData(naming_convention=convention)
@@ -18,52 +18,78 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Activity(db.Model, SerializerMixin):
-    __tablename__ = 'activities'
+    __tablename__ = "activities"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
 
     # Add relationship
-    
+    signups = db.relationship("Signup", back_populates="activity")
+    campers = association_proxy("signups", "camper")
     # Add serialization rules
-    
+    serialize_rules = ("-signups.activity",)
+
     def __repr__(self):
-        return f'<Activity {self.id}: {self.name}>'
+        return f"<Activity {self.id}: {self.name}>"
 
 
 class Camper(db.Model, SerializerMixin):
-    __tablename__ = 'campers'
+    __tablename__ = "campers"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
 
     # Add relationship
-    
+    signups = db.relationship("Signup", back_populates="camper")
+    activities = association_proxy("signups", "activity")
     # Add serialization rules
-    
+    serialize_rules = ("-signups.camper",)
+
     # Add validation
-    
-    
+    @validates("name")
+    def validates_name(self, key, new_name):
+        if not new_name:
+            raise ValueError("validation errors")
+        return new_name
+
+    @validates("age")
+    def validates_age(self, key, new_age):
+        if not new_age or new_age < 8 or new_age > 18:
+            raise ValueError("validation errors")
+        return new_age
+
     def __repr__(self):
-        return f'<Camper {self.id}: {self.name}>'
+        return f"<Camper {self.id}: {self.name}>"
 
 
 class Signup(db.Model, SerializerMixin):
-    __tablename__ = 'signups'
+    __tablename__ = "signups"
 
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.Integer)
+    camper_id = db.Column(db.Integer, db.ForeignKey("campers.id"), nullable=False)
+    activity_id = db.Column(db.Integer, db.ForeignKey("activities.id"), nullable=False)
 
     # Add relationships
-    
+    camper = db.relationship("Camper", back_populates="signups")
+    activity = db.relationship("Activity", back_populates="signups")
     # Add serialization rules
-    
+    serialize_rules = (
+        "-camper.signups",
+        "-activity.signups",
+    )
+
     # Add validation
-    
+    @validates("time")
+    def validates_time(self, key, new_time):
+        if new_time < 0 or new_time > 23:
+            raise ValueError("validation errors")
+        return new_time
+
     def __repr__(self):
-        return f'<Signup {self.id}>'
+        return f"<Signup {self.id}>"
 
 
 # add any models you may need.
